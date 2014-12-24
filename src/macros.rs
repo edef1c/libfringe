5f28b8592a087ddeca0d4368a67cc5820dacf1d8
Nathan Zadoks <nathan@nathan7.eu>
2014-12-24 06:22:49 +0100

    factor macros out and generalise them

---
+++ src/macros.rs
@@ -0,0 +1,29 @@
+macro_rules! __into_fields {
+  ($x:ident { $field:ident } <- $iter:ident) => {
+    match $iter.next() {
+      Some(value) => {
+        $x.$field = *value;
+        Some($iter)
+      }
+      None => None
+    }
+  };
+  ($x:ident { $field:ident, $($fields_rest:ident),* } <- $iter:ident) => {
+    match $iter.next() {
+      Some(value) => {
+        $x.$field = *value;
+        __into_fields!($x { $($fields_rest),* } <- $iter)
+      }
+      None => None
+    }
+  };
+}
+
+macro_rules! into_fields {
+  ($x:ident { $($fields_rest:ident),* } <- $iter:expr) => {
+    {
+      let mut iter = $iter;
+      __into_fields!($x { $($fields_rest),* } <- iter)
+    }
+  }
+}
