2ceda0d80fc79f4779bc35721f1b87ce5a6c73b9
Nathan Zadoks <nathan@nathan7.eu>
2015-03-27 03:31:02 -0400

    document the assembly

--- src/swap.s
+++ src/swap.s
@@ -1,17 +1,37 @@
+/// switch to a new context
+/// arguments: rdi: stack pointer pointer
+
+// make sure we leave the red zone alone
 sub $$128, %rsp
+
+// save the Rust stack limit and the frame pointer, respectively
 pushq %fs:0x70
 pushq %rbp
-call 1f
 
+// save the return address to the stack, control flow continues at label 1
+call 1f
+// we arrive here once this context is reactivated
+
+// restore the frame pointer and the Rust stack limit, respectively
 popq %rbp
 popq %fs:0x70
+
+// give back the red zone
 add $$128, %rsp
+
+// and we merrily go on our way, back into Rust-land
 jmp 2f
 
 1:
+  // retrieve the new stack pointer
   movq (%rdi), %rax
+  // save the old stack pointer
   movq %rsp, (%rdi)
+  // switch to the new stack pointer
   movq %rax, %rsp
+
+  // jump into the new context (return to the call point)
   popq %rax
   jmpq *%rax
+
 2:
