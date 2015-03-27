884496c6b9a22f164ebffd5a072814f8f54de13f
edef <edef@edef.eu>
2015-03-27 03:31:02 -0400

    document the assembly

--- src/init.s
+++ src/init.s
@@ -1,17 +1,36 @@
+/// initialise a new context
+/// arguments: rdi: stack pointer,
+///            rsi: function pointer,
+///            rdx, data pointer
+///            rcx, stack limit
+
+// switch to the fresh stack
 xchg %rsp, %rdi
 
+// save the function pointer, data pointer, and stack limit, respectively
 pushq %rsi
 pushq %rdx
 pushq %rcx
-call 1f
 
+// save the return address, control flow continues at label 1
+call 1f
+// we arrive here once this context is reactivated (see swap.s)
+
+// restore the stack limit, data pointer, and function pointer, respectively
 popq %fs:0x70
 popq %rdi
 popq %rax
 
+// initialise the frame pointer
 movq $$0, %rbp
+
+// call the function pointer with the data pointer (rdi is the first argument)
 call *%rax
+
+// crash if it ever returns
 ud2
 
 1:
+  // save our neatly-setup new stack
   xchg %rsp, %rdi
+  // back into Rust-land we go
