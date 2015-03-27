a9782d091819ec8a4f16f018d0ccd4a014614d39
Nathan Zadoks <nathan@nathan7.eu>
2015-03-26 22:48:51 -0400

    simplify build script

--- build.rs
+++ build.rs
@@ -1,47 +1,5 @@
-#![feature(old_io, old_path)]
-use std::old_io::Command;
-use std::old_io::fs::PathExtensions;
-use std::old_io::fs;
-use std::env;
+extern crate gcc;
 
 fn main() {
-  let out_dir = env::var("OUT_DIR").unwrap();
-  let mut objects = Vec::new();
-
-  let files = fs::readdir(&Path::new("src")).unwrap();
-  let files = files.iter().filter(|p| p.is_file());
-
-  for file in files {
-    if let Some(filename) = file.filename_str() {
-      let filepath = format!("src/{}", filename);
-      let outpath;
-
-      if let Some(basename) = eat_extension(filename, ".c") {
-        outpath = format!("{}/{}.o", out_dir, basename);
-
-        Command::new("cc").args(&[&*filepath, "-c", "-fPIC", "-o"])
-                          .arg(outpath.clone())
-                          .status().unwrap();
-      }
-      else { continue }
-
-      objects.push(outpath);
-    }
-  }
-
-  Command::new("ar").args(&["crus", "libcontext.a"])
-                    .args(&*objects)
-                    .cwd(&Path::new(&out_dir))
-                    .status().unwrap();
-
-  println!("cargo:rustc-flags=-L {} -l context:static", out_dir);
-}
-
-fn eat_extension<'a>(s: &'a str, ext: &str) -> Option<&'a str> {
-  if s.ends_with(ext) {
-    Some(&s[..s.len() - ext.len()])
-  }
-  else {
-    None
-  }
+  gcc::compile_library("libcontext.a", &["src/platform.c"]);
 }
