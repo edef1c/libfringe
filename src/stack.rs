2af5b3a11a3a85241f388a07703d71918d2c6b74
Nathan Zadoks <nathan@nathan7.eu>
2015-04-15 15:56:57 -0400

    add StackSource and implement it for platform::Stack

--- src/stack.rs
+++ src/stack.rs
@@ -2,3 +2,8 @@ pub trait Stack {
   fn top(&mut self) -> *mut u8;
   fn limit(&self) -> *const u8;
 }
+
+pub trait StackSource {
+  type Output: Stack;
+  fn get_stack(size: usize) -> Self::Output;
+}
