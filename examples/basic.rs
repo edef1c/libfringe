6c5ceec7c5a03cac9400276b16c69ac66ec83f29
Nathan Zadoks <nathan@nathan7.eu>
2015-04-15 16:26:51 -0400

    parametrise Context on Stack

--- examples/basic.rs
+++ examples/basic.rs
@@ -3,7 +3,7 @@ extern crate lwkt;
 use lwkt::{Context, StackSource};
 
 #[thread_local]
-static mut ctx_slot: *mut Context = 0 as *mut Context;
+static mut ctx_slot: *mut Context<lwkt::os::Stack> = 0 as *mut Context<_>;
 
 fn main() {
   unsafe {
