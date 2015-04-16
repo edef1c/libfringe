5bc9cc26f92c029074e261c916df25a0c75562ea
Nathan Zadoks <nathan@nathan7.eu>
2015-04-16 03:21:40 -0400

    make Valgrind optional

---
+++ src/debug/valgrind/mod.rs
@@ -0,0 +1,14 @@
+pub use self::imp::*;
+
+#[cfg(feature = "valgrind")]
+#[path = "native.rs"]
+mod imp;
+
+#[cfg(not(feature = "valgrind"))]
+mod imp {
+  //! Stub for the Valgrind functions
+  #![allow(non_camel_case_types)]
+  pub type stack_id_t = ();
+  pub unsafe fn stack_register(_start: *const u8, _end: *const u8) -> stack_id_t {}
+  pub unsafe fn stack_deregister(_id: stack_id_t) {}
+}
