a1dc5242c09f6b2a573ca9d81a316ca26ad3865a
Nathan Zadoks <nathan@nathan7.eu>
2015-04-15 22:41:34 -0400

    move the Valgrind doc-comments to Rust-land, where they actually matter

--- src/valgrind.rs
+++ src/valgrind.rs
@@ -1,9 +1,19 @@
 #![allow(non_camel_case_types)]
+//! In order for Valgrind to keep track of stack overflows and such, it needs
+//! a little help. That help unfortunately comes in the form of a pair of C
+//! macros. Calling out to un-inlineable C code for this is pointlessly slow,
+//! but that's the way it is for now.
 
 pub type stack_id_t = u32;
 extern "C" {
   #[link_name = "valgrind_stack_register"]
+  /// Register a stack with Valgrind. Returns an integer ID that can
+  /// be used to deregister the stack when it's deallocated.
+  /// `start < end`, though Valgrind will happily accept either.
   pub fn stack_register(start: *const u8, end: *const u8) -> stack_id_t;
+
   #[link_name = "valgrind_stack_deregister"]
+  /// Deregister a stack from Valgrind. Takes the integer ID that was returned
+  /// on registration.
   pub fn stack_deregister(id: stack_id_t);
 }
