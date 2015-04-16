904da1a1c825c58e4574600f3e2fc33446c64612
edef <edef@edef.eu>
2015-04-15 22:22:02 -0400

    use Rust types for Valgrind, and use u32 for stack IDs

--- src/platform.c
+++ src/platform.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include "valgrind/valgrind.h"
 
 // In order for Valgrind to keep track of stack overflows and such, it needs
@@ -5,14 +6,16 @@
 // macros. Calling out to un-inlineable C code for this is pointlessly slow,
 // but that's the way it is for now.
 
+typedef uint32_t valgrind_stack_id_t;
+
 // Register a stack with Valgrind. start < end. Returns an integer ID that can
 // be used to deregister the stack when it's deallocated.
-unsigned int lwt_stack_register(const void *start, const void *end) {
+valgrind_stack_id_t lwt_stack_register(const void *start, const void *end) {
   return VALGRIND_STACK_REGISTER(start, end);
 }
 
 // Deregister a stack from Valgrind. Takes the integer ID that was returned
 // on registration.
-void lwt_stack_deregister(unsigned int id) {
+void lwt_stack_deregister(valgrind_stack_id_t id) {
   VALGRIND_STACK_DEREGISTER(id);
 }
