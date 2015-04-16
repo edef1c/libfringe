d9d4110ab24c2ee5163e816a60680597456633cf
Nathan Zadoks <nathan@nathan7.eu>
2015-04-16 08:06:57 -0400

    big rename — we're libfringe now

--- benches/context_new.rs
+++ benches/context_new.rs
@@ -1,9 +1,10 @@
+// This file is part of libfringe, a low-level green threading library.
 // Copyright (c) 2015, Nathan Zadoks <nathan@nathan7.eu>
 // See the LICENSE file included in this distribution.
 #![feature(test)]
 extern crate test;
-extern crate lwkt;
-use lwkt::{Context, Stack};
+extern crate fringe;
+use fringe::{Context, Stack};
 
 static mut ctx_slot: *mut Context<'static, SliceStack<'static>> = 0 as *mut Context<_>;
 static mut stack_buf: [u8; 1024] = [0; 1024];
@@ -27,7 +28,7 @@ fn context_new(b: &mut test::Bencher) {
 }
 
 struct SliceStack<'a>(&'a mut [u8]);
-impl<'a> lwkt::Stack for SliceStack<'a> {
+impl<'a> fringe::Stack for SliceStack<'a> {
   fn top(&mut self) -> *mut u8 {
     unsafe {
       self.0.as_mut_ptr().offset(self.0.len() as isize)
--- src/stack.rs
+++ src/stack.rs
@@ -1,3 +1,4 @@
+// This file is part of libfringe, a low-level green threading library.
 // Copyright (c) 2015, Nathan Zadoks <nathan@nathan7.eu>
 // See the LICENSE file included in this distribution.
 //! Traits for stacks.
