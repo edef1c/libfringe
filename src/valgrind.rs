db6a2ff89b37b01de48f1f090405282c84859567
edef <edef@edef.eu>
2015-04-15 22:25:52 -0400

    factor Valgrind support out and expose it
    Now other Stack / StackSource implementations can use the same Valgrind
    code. Ref #3.

---
+++ src/valgrind.rs
@@ -0,0 +1,9 @@
+#![allow(non_camel_case_types)]
+
+pub type stack_id_t = u32;
+extern "C" {
+  #[link_name = "lwt_stack_register"]
+  pub fn stack_register(start: *const u8, end: *const u8) -> stack_id_t;
+  #[link_name = "lwt_stack_deregister"]
+  pub fn stack_deregister(id: stack_id_t);
+}
