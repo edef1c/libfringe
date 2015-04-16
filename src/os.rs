57b2fbdcefc52ef8921c99e5575eb6067e3a7dee
Nathan Zadoks <nathan@nathan7.eu>
2015-04-16 02:04:06 -0400

    separate all the libc interaction out per OS
    We're now handling more of this ourselves, and leaving less to libstd.
    Hopefully, we'll eventually break free of libstd, leaving a
    highly-focused low-level library that retains all its conveniences in
    freestanding environments.

--- src/os.rs
+++ src/os.rs
@@ -1,12 +1,11 @@
-extern crate libc;
 extern crate std;
-use self::std::prelude::v1::*;
-use self::std::env;
+use core::prelude::*;
 use self::std::io::Error as IoError;
-use self::libc::{c_void, size_t};
-use core::ptr;
 use stack;
 use valgrind;
+
+#[cfg(unix)]
+#[path = "os/unix.rs"] mod sys;
 
 #[allow(raw_pointer_derive)]
 #[derive(Debug)]
@@ -34,32 +33,27 @@ impl stack::Stack for Stack {
 
   fn limit(&self) -> *const u8 {
     unsafe {
-      self.ptr.offset(env::page_size() as isize)
+      self.ptr.offset(sys::page_size() as isize)
     }
   }
 }
 
 impl Stack {
   fn new(size: usize) -> Stack {
-    let page_size = env::page_size();
+    let page_size = sys::page_size();
 
     // round the page size up,
     // using the fact that it is a power of two
     let len = (size + page_size - 1) & !(page_size - 1);
 
-    const STACK_PROT: libc::c_int = libc::PROT_READ | libc::PROT_WRITE;
-    const STACK_FLAGS: libc::c_int = libc::MAP_STACK
-                                   | libc::MAP_PRIVATE
-                                   | libc::MAP_ANON;
-
     let stack = unsafe {
-      let ptr = libc::mmap(ptr::null_mut(), len as size_t,
-                           STACK_PROT, STACK_FLAGS, -1, 0);
-
-      if ptr == libc::MAP_FAILED {
-        panic!("mmap for stack of size {} failed: {:?}",
-               len, IoError::last_os_error())
-      }
+      let ptr = match sys::map_stack(size) {
+        None => {
+          panic!("mmap for stack of size {} failed: {:?}",
+                 len, IoError::last_os_error())
+        }
+        Some(ptr) => ptr
+      };
 
       let valgrind_id =
         valgrind::stack_register(ptr as *const _,
@@ -68,20 +62,14 @@ impl Stack {
       Stack { ptr: ptr as *mut u8, len: len, valgrind_id: valgrind_id }
     };
 
-    stack.protect();
-
-    stack
-  }
-
-  fn protect(&self) {
     unsafe {
-      if libc::mprotect(self.ptr as *mut c_void,
-                        env::page_size() as libc::size_t,
-                        libc::PROT_NONE) != 0 {
+      if !sys::protect_stack(stack.ptr) {
         panic!("mprotect for guard page of stack {:p} failed: {:?}",
-               self.ptr, IoError::last_os_error());
+               stack.ptr, IoError::last_os_error());
       }
     }
+
+    stack
   }
 }
 
@@ -89,7 +77,7 @@ impl Drop for Stack {
   fn drop(&mut self) {
     unsafe {
       valgrind::stack_deregister(self.valgrind_id);
-      if libc::munmap(self.ptr as *mut c_void, self.len as size_t) != 0 {
+      if !sys::unmap_stack(self.ptr, self.len) {
         panic!("munmap for stack {:p} of size {} failed: {:?}",
                self.ptr, self.len, IoError::last_os_error())
       }
