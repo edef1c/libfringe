04bbeb88f4876dcc75c0b97a745596f723a28be8
edef <edef@edef.eu>
2015-04-15 22:45:50 -0400

    add valgrind_stack_change
    Unlike valgrind_stack_register, this won't automatically swap
    if `start > end`. It's probably wiser to pretend it doesn't swap at all.

--- src/valgrind.c
+++ src/valgrind.c
@@ -7,6 +7,10 @@ valgrind_stack_id_t valgrind_stack_register(const void *start, const void *end) 
   return VALGRIND_STACK_REGISTER(start, end);
 }
 
+void valgrind_stack_change(valgrind_stack_id_t id, const void *start, const void *end) {
+  VALGRIND_STACK_CHANGE(id, start, end);
+}
+
 void valgrind_stack_deregister(valgrind_stack_id_t id) {
   VALGRIND_STACK_DEREGISTER(id);
 }
