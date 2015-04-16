a389576d0f7e27ac094455978d64f28ed5aaa781
edef <edef@edef.eu>
2015-04-16 07:30:53 -0400

    document os

--- src/os/mod.rs
+++ src/os/mod.rs
@@ -1,5 +1,10 @@
 // Copyright (c) 2015, edef <edef@edef.eu>
 // See the LICENSE file included in this distribution.
+//! This module provides stacks using the operating system's
+//! anonymous memory mapping facility, usually `mmap`.
+//! The stacks it provides come with a guard page, which is not included
+//! in the stack limit.
+
 extern crate std;
 use core::prelude::*;
 use self::std::io::Error as IoError;
