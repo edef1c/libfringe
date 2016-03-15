817ecbb3180b25600212a6f343307a5f81e3f277
Nathan Zadoks <nathan@nathan7.eu>
2016-03-16 00:14:09 +0100

    remove feature attributes for stable stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 // This file is part of libfringe, a low-level green threading library.
 // Copyright (c) 2015, Nathan Zadoks <nathan@nathan7.eu>
 // See the LICENSE file included in this distribution.
-#![feature(no_std)]
-#![feature(asm, core)]
+#![feature(asm)]
 #![no_std]
 
 //! libfringe is a low-level green threading library.
