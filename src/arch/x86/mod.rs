0a159ed6ee2ab71e1510bc80e1e4a49e21cc06e4
Nathan Zadoks <nathan@nathan7.eu>
2016-03-22 08:25:23 +0100

    remove copyright years
    They're obnoxious to update, and I'm told they have no legal value.

--- src/arch/x86/mod.rs
+++ src/arch/x86/mod.rs
@@ -1,5 +1,5 @@
 // This file is part of libfringe, a low-level green threading library.
-// Copyright (c) 2015, Nathan Zadoks <nathan@nathan7.eu>
+// Copyright (c) Nathan Zadoks <nathan@nathan7.eu>
 // See the LICENSE file included in this distribution.
 use void::Void;
 
