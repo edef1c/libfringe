b79e7c0a9bbeda2c285a7551c1206e7ea48c747c
whitequark <whitequark@whitequark.org>
2016-08-11 23:18:36 +0000

    Rename Stack::top() to Stack::base(), which is correct.
    Fixes #25.

--- src/arch/x86.rs
+++ src/arch/x86.rs
@@ -84,7 +84,7 @@ pub unsafe fn init(stack: &Stack, f: unsafe extern "C" fn(usize) -> !) -> StackP
     *sp.0 = val
   }
 
-  let mut sp = StackPointer(stack.top() as *mut usize);
+  let mut sp = StackPointer(stack.base() as *mut usize);
   push(&mut sp, 0xdead0cfa); // CFA slot
   push(&mut sp, f as usize); // function
   push(&mut sp, trampoline_1 as usize);
@@ -96,7 +96,7 @@ pub unsafe fn init(stack: &Stack, f: unsafe extern "C" fn(usize) -> !) -> StackP
 pub unsafe fn swap(arg: usize, old_sp: &mut StackPointer, new_sp: &StackPointer,
                    new_stack: &Stack) -> usize {
   // Address of the topmost CFA stack slot.
-  let new_cfa = (new_stack.top() as *mut usize).offset(-1);
+  let new_cfa = (new_stack.base() as *mut usize).offset(-1);
 
   #[naked]
   unsafe extern "C" fn trampoline() {
