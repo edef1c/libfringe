1cb71d1a49c60a0ef006106257898c87cc4559c2
whitequark <whitequark@whitequark.org>
2016-08-11 01:34:53 +0000

    Remove workaround for rust-lang/rust#25544.

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -2,17 +2,10 @@
 // Copyright (c) Nathan Zadoks <nathan@nathan7.eu>,
 //               whitequark <whitequark@whitequark.org>
 // See the LICENSE file included in this distribution.
+
 pub use self::imp::*;
 
-// rust-lang/rust#25544
-// #[cfg_attr(target_arch = "x86",    path = "x86.rs")]
-// #[cfg_attr(target_arch = "x86_64", path = "x86_64.rs")]
-// mod imp;
-
-#[cfg(target_arch = "x86")]
-#[path = "x86.rs"]
-mod imp;
-
-#[cfg(target_arch = "x86_64")]
-#[path = "x86_64.rs"]
+#[allow(unused_attributes)] // rust-lang/rust#35584
+#[cfg_attr(target_arch = "x86",    path = "x86.rs")]
+#[cfg_attr(target_arch = "x86_64", path = "x86_64.rs")]
 mod imp;
