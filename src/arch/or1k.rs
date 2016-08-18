e0ad79ea0c1187b8be43394bb9a35176a3e223e1
whitequark <whitequark@whitequark.org>
2016-08-18 19:35:30 +0000

    Add an OR1K port.

--- src/arch/or1k.rs
+++ src/arch/or1k.rs
@@ -3,17 +3,12 @@
 //               whitequark <whitequark@whitequark.org>
 // See the LICENSE file included in this distribution.
 
-// To understand the code in this file, keep in mind these two facts:
-// * x86_64 SysV C ABI has a "red zone": 128 bytes under the top of the stack
+// To understand the machine code in this file, keep in mind these facts:
+// * OR1K C ABI has a "red zone": 128 bytes under the top of the stack
 //   that is defined to be unmolested by signal handlers, interrupts, etc.
-//   Leaf functions can use the red zone without adjusting rsp or rbp.
-// * x86_64 SysV C ABI requires the stack to be aligned at function entry,
-//   so that (%rsp+8) is a multiple of 16. Aligned operands are a requirement
-//   of SIMD instructions, and making this the responsibility of the caller
-//   avoids having to maintain a frame pointer, which is necessary when
-//   a function has to realign the stack from an unknown state.
-// * x86_64 SysV C ABI passes the first argument in %rdi. We also use %rdi
-//   to pass a value while swapping context; this is an arbitrary choice
+//   Leaf functions can use the red zone without adjusting r1 or r2.
+// * OR1K C ABI passes the first argument in r3. We also use r3 to pass a value
+//   while swapping context; this is an arbitrary choice
 //   (we clobber all registers and could use any of them) but this allows us
 //   to reuse the swap function to perform the initial call.
 //
@@ -35,10 +30,10 @@
 //
 // A high-level overview of the function of the trampolines is:
 // * The 2nd init trampoline puts a controlled value (written in swap to `new_cfa`)
-//   into %rbx.
-// * The 1st init trampoline tells the unwinder to set %rsp to %rbx, thus continuing
+//   into r13.
+// * The 1st init trampoline tells the unwinder to set r1 to r13, thus continuing
 //   unwinding at the swap call site instead of falling off the end of context stack.
-// * The 1st init trampoline together with the swap trampoline also restore %rbp
+// * The 1st init trampoline together with the swap trampoline also restore r2
 //   when unwinding as well as returning normally, because LLVM does not do it for us.
 use stack::Stack;
 
@@ -60,13 +55,15 @@ pub unsafe fn init(stack: &Stack, f: unsafe extern "C" fn(usize) -> !) -> StackP
 
         # Set up the first part of our DWARF CFI linking stacks together.
         # When unwinding the frame corresponding to this function, a DWARF unwinder
-        # will use %rbx as the next call frame address, restore return address
-        # from CFA-8 and restore %rbp from CFA-16. This mirrors what the second half
-        # of `swap_trampoline` does.
-        .cfi_def_cfa %rbx, 0
-        .cfi_offset %rbp, -16
+        # will use r13 as the next call frame address, restore return address (r9)
+        # from CFA-4 and restore stack pointer (r2) from CFA-8.
+        # This mirrors what the second half of `swap_trampoline` does.
+        .cfi_def_cfa r13, 0
+        .cfi_offset r2, -8
+        .cfi_offset r9, -4
         # Call the next trampoline.
-        call   ${0:c}
+        l.j     ${0}
+        l.nop
 
       .Lend:
       .size __morestack, .Lend-__morestack
@@ -80,10 +77,12 @@ pub unsafe fn init(stack: &Stack, f: unsafe extern "C" fn(usize) -> !) -> StackP
       r#"
         # Set up the second part of our DWARF CFI.
         # When unwinding the frame corresponding to this function, a DWARF unwinder
-        # will restore %rbx (and thus CFA of the first trampoline) from the stack slot.
-        .cfi_offset %rbx, 16
+        # will restore r13 (and thus CFA of the first trampoline) from the stack slot.
+        .cfi_offset r13, 4
         # Call the provided function.
-        call    *8(%rsp)
+        l.lwz   r9, 0(r1)
+        l.jr    r9
+        l.nop
       "#
       : : : "memory" : "volatile")
   }
@@ -94,12 +93,12 @@ pub unsafe fn init(stack: &Stack, f: unsafe extern "C" fn(usize) -> !) -> StackP
   }
 
   let mut sp = StackPointer(stack.base() as *mut usize);
-  push(&mut sp, 0xdeaddeaddead0cfa); // CFA slot
-  push(&mut sp, 0 as usize); // alignment
-  push(&mut sp, f as usize); // function
-  push(&mut sp, trampoline_1 as usize);
-  push(&mut sp, 0xdeaddeaddeadbbbb); // saved %rbp
-  sp
+  push(&mut sp, 0xdead0cfa);            // CFA slot
+  push(&mut sp, f as usize);            // function
+  let rsp = sp.clone();
+  push(&mut sp, trampoline_1 as usize); // saved r9
+  push(&mut sp, 0xdeadbbbb);            // saved r2
+  rsp
 }
 
 #[inline(always)]
@@ -112,25 +111,28 @@ pub unsafe fn swap(arg: usize, old_sp: &mut StackPointer, new_sp: &StackPointer,
   unsafe extern "C" fn trampoline() {
     asm!(
       r#"
+        # Save instruction pointer of the old context.
+        l.sw    -4(r1), r9
+
         # Save frame pointer explicitly; the unwinder uses it to find CFA of
         # the caller, and so it has to have the correct value immediately after
         # the call instruction that invoked the trampoline.
-        pushq   %rbp
+        l.sw    -8(r1), r2
 
-        # Remember stack pointer of the old context, in case %rdx==%rsi.
-        movq    %rsp, %rbx
+        # Remember stack pointer of the old context, in case r5==r4.
+        l.or    r13, r0, r1
         # Load stack pointer of the new context.
-        movq    (%rdx), %rsp
+        l.lwz   r1, 0(r5)
         # Save stack pointer of the old context.
-        movq    %rbx, (%rsi)
+        l.sw    0(r4), r13
 
         # Restore frame pointer of the new context.
-        popq    %rbp
+        l.lwz   r2, -8(r1)
 
-        # Return into the new context. Use `pop` and `jmp` instead of a `ret`
-        # to avoid return address mispredictions (~8ns per `ret` on Ivy Bridge).
-        popq    %rbx
-        jmpq    *%rbx
+        # Return into the new context.
+        l.lwz   r9, -4(r1)
+        l.jr    r9
+        l.nop
       "#
       : : : "memory" : "volatile")
   }
@@ -139,29 +141,23 @@ pub unsafe fn swap(arg: usize, old_sp: &mut StackPointer, new_sp: &StackPointer,
   asm!(
     r#"
       # Link the call stacks together.
-      movq    %rsp, (%rcx)
-      # Push instruction pointer of the old context and switch to
+      l.sw    0(r6), r1
+      # Put instruction pointer of the old context into r9 and switch to
       # the new context.
-      call    ${1:c}
+      l.jal   ${1}
+      l.nop
     "#
-    : "={rdi}" (ret)
+    : "={r3}" (ret)
     : "s" (trampoline as usize)
-      "{rdi}" (arg)
-      "{rsi}" (old_sp)
-      "{rdx}" (new_sp)
-      "{rcx}" (new_cfa)
-    : "rax",   "rbx",   "rcx",   "rdx",   "rsi",   "rdi", //"rbp",   "rsp",
-      "r8",    "r9",    "r10",   "r11",   "r12",   "r13",   "r14",   "r15",
-      "xmm0",  "xmm1",  "xmm2",  "xmm3",  "xmm4",  "xmm5",  "xmm6",  "xmm7",
-      "xmm8",  "xmm9",  "xmm10", "xmm11", "xmm12", "xmm13", "xmm14", "xmm15",
-      "xmm16", "xmm17", "xmm18", "xmm19", "xmm20", "xmm21", "xmm22", "xmm23",
-      "xmm24", "xmm25", "xmm26", "xmm27", "xmm28", "xmm29", "xmm30", "xmm31"
-      "cc", "fpsr", "flags", "memory"
-      // Ideally, we would set the LLVM "noredzone" attribute on this function
-      // (and it would be propagated to the call site). Unfortunately, rustc
-      // provides no such functionality. Fortunately, by a lucky coincidence,
-      // the "alignstack" LLVM inline assembly option does exactly the same
-      // thing on x86_64.
-    : "volatile", "alignstack");
+      "{r3}" (arg)
+      "{r4}" (old_sp)
+      "{r5}" (new_sp)
+      "{r6}" (new_cfa)
+    :                      "r3",  "r4",  "r5",  "r6",  "r7",
+      "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15",
+      "r16", "r17", "r18", "r19", "r20", "r21", "r22", "r23",
+      "r24", "r25", "r26", "r27", "r28", "r29", "r30", "r31",
+      "flags", "memory"
+    : "volatile");
   ret
 }
