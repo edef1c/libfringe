7848ae03c4b7424f6171732916e401c0bda265f7
whitequark <whitequark@whitequark.org>
2016-08-19 09:18:46 +0000

    Implement SliceStack.

--- tests/stack.rs
+++ tests/stack.rs
@@ -3,17 +3,24 @@
 // See the LICENSE file included in this distribution.
 extern crate fringe;
 
-use fringe::{Stack, OsStack};
+use fringe::{Stack, SliceStack, OsStack};
 
 #[test]
-fn default_stack() {
+fn slice_stack() {
+    let mut memory = [0; 1024];
+    let stack = SliceStack(&mut memory);
+    assert_eq!(stack.base() as isize - stack.limit() as isize, 1024);
+}
+
+#[test]
+fn default_os_stack() {
   let stack = OsStack::new(0).unwrap();
   // Make sure the topmost page of the stack, at least, is accessible.
   unsafe { *(stack.base().offset(-1)) = 0; }
 }
 
 #[test]
-fn one_page_stack() {
+fn one_page_os_stack() {
   let stack = OsStack::new(4096).unwrap();
   // Make sure the topmost page of the stack, at least, is accessible.
   unsafe { *(stack.base().offset(-1)) = 0; }
