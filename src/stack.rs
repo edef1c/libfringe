f8cf95f686376223847baa07e383f148b76ea1f9
whitequark <whitequark@whitequark.org>
2016-08-19 09:18:46 +0000

    Implement SliceStack.

--- src/stack.rs
+++ src/stack.rs
@@ -1,7 +1,6 @@
 // This file is part of libfringe, a low-level green threading library.
 // Copyright (c) edef <edef@edef.eu>
 // See the LICENSE file included in this distribution.
-//! Traits for stacks.
 
 /// A trait for objects that hold ownership of a stack.
 pub trait Stack {
@@ -20,3 +19,22 @@ pub trait Stack {
 /// A guarded stack must guarantee that any access of data at addresses `limit()` to
 /// `limit().offset(4096)` will abnormally terminate the program.
 pub unsafe trait GuardedStack {}
+
+/// SliceStack holds a non-guarded stack allocated elsewhere and provided as a mutable
+/// slice.
+pub struct SliceStack<'a>(pub &'a mut [u8]);
+
+impl<'a> Stack for SliceStack<'a> {
+    #[inline(always)]
+    fn base(&self) -> *mut u8 {
+        // The slice cannot wrap around the address space, so the conversion from usize
+        // to isize will not wrap either.
+        let len: isize = self.0.len() as isize;
+        unsafe { self.limit().offset(len) }
+    }
+
+    #[inline(always)]
+    fn limit(&self) -> *mut u8 {
+        self.0.as_ptr() as *mut u8
+    }
+}
