c8c06c43e8bfeede5f1a60fbd72a7f6970f66e3c
Nathan Zadoks <nathan@nathan7.eu>
2016-08-20 23:45:01 +0200

    Update the license headers for MIT/Apache

--- src/arch/x86.rs
+++ src/arch/x86.rs
@@ -1,7 +1,10 @@
 // This file is part of libfringe, a low-level green threading library.
 // Copyright (c) Nathan Zadoks <nathan@nathan7.eu>,
 //               whitequark <whitequark@whitequark.org>
-// See the LICENSE file included in this distribution.
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
 
 // To understand the machine code in this file, keep in mind these facts:
 // * i686 SysV C ABI requires the stack to be aligned at function entry,
--- src/stack.rs
+++ src/stack.rs
@@ -1,6 +1,9 @@
 // This file is part of libfringe, a low-level green threading library.
 // Copyright (c) Nathan Zadoks <nathan@nathan7.eu>
-// See the LICENSE file included in this distribution.
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
 
 /// A trait for objects that hold ownership of a stack.
 pub trait Stack {
