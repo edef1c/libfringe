48512d0e4ecc913be7a56f94825ba856c5a1e8aa
whitequark <whitequark@whitequark.org>
2016-08-30 10:45:39 +0000

    Do not simultaneously clobber registers passed into assembly.
    This is not actually legal (although the restriction is not
    documented anywhere), and is not caught by LLVM unless the codegen
    option -verify-machineinstrs is specified. This option is now used
    on Travis.
    
    While not legal, this does not seem to result in invalid output
    (although it creates an unnecessary spill); however, under extremely
    specific circumstances (e.g. when a register scavenger is run under
    severe pressure), this will result in a codegen crash, which is
    how I found it.

--- src/arch/x86_64.rs
+++ src/arch/x86_64.rs
@@ -153,7 +153,7 @@ pub unsafe fn swap(arg: usize, old_sp: &mut StackPointer, new_sp: &StackPointer,
       "{rsi}" (old_sp)
       "{rdx}" (new_sp)
       "{rcx}" (new_cfa)
-    : "rax",   "rbx",   "rcx",   "rdx",   "rsi",   "rdi", //"rbp",   "rsp",
+    : "rax",   "rbx", /*"rcx",   "rdx",   "rsi",   "rdi",   "rbp",   "rsp",*/
       "r8",    "r9",    "r10",   "r11",   "r12",   "r13",   "r14",   "r15",
       "xmm0",  "xmm1",  "xmm2",  "xmm3",  "xmm4",  "xmm5",  "xmm6",  "xmm7",
       "xmm8",  "xmm9",  "xmm10", "xmm11", "xmm12", "xmm13", "xmm14", "xmm15",
