fadedf5b42cae65f541eef0eb13ff3f20f72db4a
Amanieu d'Antras <amanieu@gmail.com>
2016-09-01 17:36:40 +0100

    Add missing inline asm clobbers for x86 and x86_64

--- src/arch/x86_64.rs
+++ src/arch/x86_64.rs
@@ -182,11 +182,12 @@ pub unsafe fn swap(arg: usize, old_sp: *mut StackPointer, new_sp: *const StackPo
       "{rcx}" (new_cfa)
     : "rax",   "rbx",   "rcx",   "rdx",   "rsi", /*"rdi",   "rbp",   "rsp",*/
       "r8",    "r9",    "r10",   "r11",   "r12",   "r13",   "r14",   "r15",
+      "mm0",   "mm1",   "mm2",   "mm3",   "mm4",   "mm5",   "mm6",   "mm7",
       "xmm0",  "xmm1",  "xmm2",  "xmm3",  "xmm4",  "xmm5",  "xmm6",  "xmm7",
       "xmm8",  "xmm9",  "xmm10", "xmm11", "xmm12", "xmm13", "xmm14", "xmm15",
       "xmm16", "xmm17", "xmm18", "xmm19", "xmm20", "xmm21", "xmm22", "xmm23",
-      "xmm24", "xmm25", "xmm26", "xmm27", "xmm28", "xmm29", "xmm30", "xmm31"
-      "cc", "fpsr", "flags", "memory"
+      "xmm24", "xmm25", "xmm26", "xmm27", "xmm28", "xmm29", "xmm30", "xmm31",
+      "cc", "dirflag", "fpsr", "flags", "memory"
       // Ideally, we would set the LLVM "noredzone" attribute on this function
       // (and it would be propagated to the call site). Unfortunately, rustc
       // provides no such functionality. Fortunately, by a lucky coincidence,
