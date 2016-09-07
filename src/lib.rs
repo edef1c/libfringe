575cb52da8a7ef1565fb8cad059ab3ef90222522
Amanieu d'Antras <amanieu@gmail.com>
2016-09-07 20:02:41 +0100

    Pack generator inputs and outputs into usize if possible

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 // http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
-#![feature(asm, naked_functions, cfg_target_vendor, nonzero)]
+#![feature(asm, naked_functions, cfg_target_vendor, nonzero, untagged_unions)]
 #![cfg_attr(feature = "alloc", feature(alloc, heap_api))]
 #![cfg_attr(test, feature(test))]
 #![no_std]
